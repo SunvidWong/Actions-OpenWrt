ef618ccec765b50fd656012d2a1458305c739b55
SunvidWong <45497358+SunvidWong@users.noreply.github.com>
2020-06-09 04:13:55 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #=================================================
-# Description: DIY script
+Description: sunvid
 # Lisence: MIT
 # Author: P3TERX
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-sed -i 's/192.168.1.1/192.168.123.99/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.0.9.1/g' package/base-files/files/bin/config_generate
